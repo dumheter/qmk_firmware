cc51f5dd9b0e7e6ff4e840e57fc4ef2bc7b7f845
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-02-14 10:52:03 -0800

    Changed a few keys for qwerty layer.

--- keyboards/xd75/keymaps/dumheter/keymap.c
+++ keyboards/xd75/keymaps/dumheter/keymap.c
@@ -63,13 +63,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  /* OLD
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | F1     | F2     | F3     | F4     | F5     | F6     | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
+ * | F1     | F1     | F2     | F3     | F4     | F5     | F6     | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
+ * | TAB    |        | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      |        | RESET  |        |        |        |
+ * | LCTL   |        | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      |        | RESET  |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   |        |        |        |        |        |
+ * | LSHIFT |        | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        | FN     | RGB TG | RGB RMD| RGB MD | P0     |        | P.     | PENT   | PENT   |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
@@ -77,9 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_FN] = { /* FUNCTION */
   { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12   },
-  { KC_MSEL, KC_UP  , KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_UP,   KC_UP,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS  },
-  { KC_LEFT, KC_DOWN, KC_RGHT, NO_LCBR, NO_RCBR, RGB_SAI, KC_LEFT, KC_LEFT, KC_DOWN, KC_RGHT, _______, RESET,   _______, _______, _______  },
-  { KC_VOLD, KC_MUTE, KC_VOLU, NO_LBRC, NO_RBRC, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______  },
+  { KC_TAB,  KC_UP  , KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_UP,   KC_UP,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS  },
+  { KC_LCTL, KC_DOWN, KC_RGHT, NO_LCBR, NO_RCBR, RGB_SAI, KC_LEFT, KC_LEFT, KC_DOWN, KC_RGHT, _______, RESET,   _______, _______, _______  },
+  { KC_LSFT, _______, KC_VOLU, NO_LBRC, NO_RBRC, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______  },
   { _______, _______, RGB_TOG, MO(_FN), RGB_RMOD,RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT,  _______, _______, _______, _______  },
  },
 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  |        | RALT   | RGUI   | RCTRL  |
+ * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | BSPACE | LALT   | ENTER  | ALTGR  | RGUI   | SPACE  |        | RALT   | RGUI   | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    NO_AA,   NO_QUOT, NO_ACUT, KC_VOLU  },
   { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NO_OSLH, NO_AE,   KC_BSLS, KC_GRV,  KC_VOLD  },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, KC_UP,   _______  },
-  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT  },
+  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_BSPC, KC_LALT, KC_ENT,  SE_ALGR, KC_RGUI, KC_RCTL, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 };
 
