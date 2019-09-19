49730abbbdda7edd2405834a7f55fef4bc9bbc9a
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-09-19 16:24:57 +0200

    Added home end keys

--- keyboards/xd75/keymaps/dumheter/keymap.c
+++ keyboards/xd75/keymaps/dumheter/keymap.c
@@ -26,7 +26,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
+/* COLEMAK
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | +?\    | prev m | next m | PLY/PS |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | LCTRL  | A      | R      | S      | T      | D      | H      | N      | E      | I      | O      | Ä      | '*´    | §½     |vol down|
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | K      | M      | ,;     | .:     | -_̣     | RSHIFT |        |  UP    |        |
+ * | LSHIFT | Z      | X      | C      | V      | B      | K      | M      | ,;     | .:     | -_̣     | RSHIFT | HOME   |  UP    | END    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
  * | <>|    | LGUI   | LALT   | FN     | SPACE  | BSPACE | LALT   | ENTER  | ALTGR  | RGUI   | DEL    | QWERTY | LEFT   | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,KC_MPRV, KC_MNXT, KC_MPLY  },
      { KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    NO_OSLH, NO_AA,   NO_QUOT, NO_ACUT, KC_VOLU  },
      { KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    NO_AE,   KC_BSLS, KC_GRV,  KC_VOLD  },
-     { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, KC_UP,   _______  },
+     { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_HOME, KC_UP,   KC_END   },
      { KC_NUBS, KC_LGUI, KC_LALT, MO(_FN), KC_SPC, KC_BSPC,  KC_LALT, KC_ENT,  SE_ALGR, KC_RGUI, KC_DEL,  TG(_QT), KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  _______, _______, _______, _______, RGB_HUD, RGB_HUI, NO_LCBR, KC_UP,   NO_RCBR, KC_F11,   KC_P7,   KC_P8,   KC_P9,   KC_PLUS  },
   { KC_LCTL, _______, _______, NO_LCBR, NO_RCBR, RGB_SAD, RGB_SAI, KC_LEFT, KC_DOWN, KC_RGHT, KC_F12,   KC_P4,   KC_P5,   KC_P6,   KC_PLUS  },
   { KC_LSFT, _______, _______, NO_LBRC, NO_RBRC, RGB_VAD, RGB_VAI, NO_LBRC, KC_BSLS, NO_RBRC, KC_PSCR,  KC_P1,   KC_P2,   KC_P3,   KC_ENT  },
-  { _______, KC_LGUI, KC_LALT, MO(_FN), RGB_TOG, RGB_RMOD,RGB_MOD, _______, SE_ALGR, KC_INS,  RESET,    KC_P0,   KC_P0,   KC_PDOT, KC_PENT  },
+  { _______, KC_LGUI, KC_LALT, KC_TRNS, RGB_TOG, RGB_RMOD,RGB_MOD, _______, SE_ALGR, KC_INS,  RESET,    KC_P0,   KC_P0,   KC_PDOT, KC_PENT  },
  },
 
 /* QWERTY
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,KC_MPRV, KC_MNXT, KC_MPLY  },
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    NO_AA,   NO_QUOT, NO_ACUT, KC_VOLU  },
   { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NO_OSLH, NO_AE,   KC_BSLS, KC_GRV,  KC_VOLD  },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, KC_UP,   _______  },
+  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_HOME, KC_UP,   KC_END   },
   { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_BSPC, KC_LALT, KC_ENT,  SE_ALGR, KC_RGUI, KC_RCTL, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 };
